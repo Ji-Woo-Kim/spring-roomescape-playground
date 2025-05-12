0852487f704a1db1395da6a4fbfd4502bba3ab18
Jiwoo Kim <ziuoo0424@gmail.com>
2025-05-13 01:58:27 +0900

    refactor: 리뷰 반영 - 예약 페이지 랜더링 메서드(GET) - 중복 검증 로직 삭제 - 테스트1-2단계용 임시 데이터셋 제거

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -3,55 +3,44 @@ package roomescape.controller;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import roomescape.domain.Reservation;
-import roomescape.dto.ReservationRequestDto;
-import roomescape.dto.ReservationResponseDto;
-import roomescape.exception.InvalidReservationRequestException;
-import roomescape.exception.NotFoundReservationException;
+import roomescape.domain.*;
+import roomescape.dto.*;
+import roomescape.exception.*;
 
 import java.net.URI;
-import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicLong;
-import java.util.stream.Collectors;
 
-@RequestMapping(value = "/reservations", produces = "application/json; charset=UTF-8")
 @RestController
 public class ReservationController {
 
     private List<Reservation> reservations = new ArrayList<>();
     private AtomicLong index = new AtomicLong(1);
 
-    public ReservationController() {
-        reservations.add(new Reservation(1L, "브라운", LocalDate.of(2023, 1, 1), LocalTime.of(10, 0)));
-        reservations.add(new Reservation(2L, "브라운", LocalDate.of(2023, 1, 2), LocalTime.of(11, 0)));
-        reservations.add(new Reservation(3L, "브라운", LocalDate.of(2023, 1, 3), LocalTime.of(12, 0)));
+    @GetMapping("/reservation")
+    public String reservationPage() {
+        return "reservation";
     }
 
-    @GetMapping
+    @GetMapping("/reservations")
     public ResponseEntity<List<ReservationResponseDto>> read() {
         List<ReservationResponseDto> responseDtos = reservations.stream()
                 .map(ReservationResponseDto::new)
-                .collect(Collectors.toList());
+                .toList();
 
         return ResponseEntity.ok(responseDtos);
     }
 
-    @PostMapping
+    @PostMapping("/reservations")
     public ResponseEntity<ReservationResponseDto> create(@Valid @RequestBody ReservationRequestDto requestDto) {
-        if (requestDto.getName().isEmpty() || requestDto.getDate() == null || requestDto.getTime() == null) {
-            throw new InvalidReservationRequestException("필수 정보가 누락되었습니다.");
-        }
-
-        Reservation newReservation = requestDto.toEntity(index.getAndIncrement());
+        Reservation newReservation = Reservation.fromDto(index.getAndIncrement(), requestDto);
         reservations.add(newReservation);
 
         URI location = URI.create("/reservations/" + newReservation.getId());
         return ResponseEntity.created(location).body(new ReservationResponseDto(newReservation));
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/reservations/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         Reservation reservation = reservations.stream()
                 .filter(it -> Objects.equals(it.getId(), id))
