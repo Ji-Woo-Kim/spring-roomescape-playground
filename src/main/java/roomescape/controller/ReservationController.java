d4b4dfd40d09e5ef284c3e1a4957dddbd9f31a96
Jiwoo Kim <ziuoo0424@gmail.com>
2025-05-15 15:39:02 +0900

    feat: Service 로직 구현

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -1,24 +1,19 @@
 package roomescape.controller;
 
-import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import roomescape.dao.ReservationDAO;
-import roomescape.domain.*;
 import roomescape.dto.*;
-import roomescape.exception.reservation.NotReservationFoundException;
+import roomescape.service.ReservationService;
 
-import java.net.URI;
 import java.util.*;
-import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class ReservationController {
 
-    private final ReservationDAO reservationDAO;
+    private final ReservationService reservationService;
 
-    public ReservationController(ReservationDAO reservationDAO) {
-        this.reservationDAO = reservationDAO;
+    public ReservationController(ReservationService reservationService) {
+        this.reservationService = reservationService;
     }
 
     @GetMapping("/reservation")
@@ -27,32 +22,28 @@ public class ReservationController {
     }
 
     @GetMapping("/reservations") //6단계 반영
-    public ResponseEntity<List<ReservationResponseDto>> read() {
-        List<ReservationResponseDto> responseDtos = reservationDAO.findAll().stream()
-                .map(ReservationResponseDto::new)
-                .toList();
-
-        return ResponseEntity.ok(responseDtos);
+    public ResponseEntity<List<ReservationResponseDto>> findAll() {
+        return ResponseEntity.ok(reservationService.findAllReservations());
     }
 
-    @PostMapping("/reservations")
-    public ResponseEntity<ReservationResponseDto> create(@Valid @RequestBody ReservationRequestDto requestDto) {
-        Reservation newReservation = Reservation.fromDto(index.getAndIncrement(), requestDto);
-        reservations.add(newReservation);
-
-        URI location = URI.create("/reservations/" + newReservation.getId());
-        return ResponseEntity.created(location).body(new ReservationResponseDto(newReservation));
-    }
-
-    @DeleteMapping("/reservations/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Long id) {
-        Reservation reservation = reservations.stream()
-                .filter(it -> Objects.equals(it.getId(), id))
-                .findFirst()
-                .orElseThrow(() -> new NotReservationFoundException("Reservation not found"));
-
-        reservations.remove(reservation);
-
-        return ResponseEntity.noContent().build();
-    }
+//    @PostMapping("/reservations")
+//    public ResponseEntity<ReservationResponseDto> create(@Valid @RequestBody ReservationRequestDto requestDto) {
+//        Reservation newReservation = Reservation.fromDto(index.getAndIncrement(), requestDto);
+//        reservations.add(newReservation);
+//
+//        URI location = URI.create("/reservations/" + newReservation.getId());
+//        return ResponseEntity.created(location).body(new ReservationResponseDto(newReservation));
+//    }
+//
+//    @DeleteMapping("/reservations/{id}")
+//    public ResponseEntity<Void> delete(@PathVariable Long id) {
+//        Reservation reservation = reservations.stream()
+//                .filter(it -> Objects.equals(it.getId(), id))
+//                .findFirst()
+//                .orElseThrow(() -> new NotReservationFoundException("Reservation not found"));
+//
+//        reservations.remove(reservation);
+//
+//        return ResponseEntity.noContent().build();
+//    }
 }
